4a7a86c72e434f077ad688ec60018a43edb681c0
Michelle <michellehsieh@yahoo.co.uk>
2019-10-31 20:41:27 -0400

    created a method for the click to represent the right ghost and added pictures

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React, {Component} from 'react';
+import Ghost from './Ghost';
+import casper from './images/casper.jpeg';
+import ghost1 from './images/ghost1.png'
+import ghost-ads-image from './images/ghosts-ads-image.jpg';
+import marshmallowghost from './images/marshmallowghost.jpeg';
+
 
 class Selector extends Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedGhost: 'casper'
+    }
+  }
+
+  selectGhost = (ghostName) => {
+    this.setState({
+      selectedGhost: ghostName,
+    });
+  };
+
+
   render () {
     return(
       <div className="container">
       <div className="navbar">
         <div> Selected: </div>
-        <div> What goes here? </div>
+        <div> {this.state.selectedGhost} </div>
       </div>
-      <div className="ghost-list">Put the Ghost compnent here</div>
+      <div className="ghost-list">Put the Ghost compnent here
+      <Ghost ghost={ghost1} selectGhost = {this.selectGhost}/>
+      <Ghost ghost={casper} selectGhost = {this.selectGhost}/>
+      <Ghost ghost={marshmallowghost} selectGhost = {this.selectGhost}/>
+      <Ghost ghost={ghost-ads-image} selectGhost = {this.selectGhost}/>
+      </div>
       </div>
     )
   }
